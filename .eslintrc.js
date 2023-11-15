4dbd2a0f5b477fde669717fda9af569c39d3dbf0
Mike Laffy <nhlflash22@gmail.com>
2023-11-14 23:59:05 -0500

    setup directory

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+// .eslintrc.js example
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+}
