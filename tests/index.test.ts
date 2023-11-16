cc36747bb55b311e75fe61de2e535c363cae7fff
Mike Laffy <nhlflash22@gmail.com>
2023-11-16 01:09:46 +0000

    Add tests for scoreboard and details

--- tests/index.test.ts
+++
@@ -1,9 +0,0 @@
-/** jest test file to test src/index.ts */
-
-import { helloWorld } from '../lib/index'
-
-describe('index module', () => {
-  test('helloWorld', () => {
-    expect(helloWorld()).toBe('Hello World')
-  })
-})
