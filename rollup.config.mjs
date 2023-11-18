ebc3332ac9c324e4f24b72c0bdd2c5530e54aea8
Mike Laffy <nhlflash22@gmail.com>
2023-11-18 22:50:57 +0000

    attempt to fix package import

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,15 +1,42 @@
+// rollup.config.js
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+
 import typescript from 'rollup-plugin-typescript2'
+import commonjs from '@rollup/plugin-commonjs';
 
-const config = [
-  {
-    input: 'lib/index.ts',
-    output: {
-      file: 'build/index.js',
-      sourcemap: true,
-    },
-    external: ['axios'],
-    plugins: [typescript()],
+const config = {
+  input: 'lib/index.ts',
+  output: {
+    file: 'build/index.js',
+    sourcemap: true,
+    format: 'cjs',
   },
-]
+  external: ['axios'],
+  plugins: [
+    commonjs(), 
+    typescript(),
+  ],
+}
+export default config;
 
-export default config
+
+// const config_ts = [
+//   {
+//     file: 'rollup.config.ts',
+//     input: 'lib/index.ts',
+//     output: {
+//       file: 'build/index.js',
+//       sourcemap: true,
+//       format: 'cjs',
+//     },
+//     external: ['axios'],
+//     plugins: [
+//       commonjs(), 
+//       typescript(),
+//     ],
+//   },
+// ]
+
+// export default config_ts
